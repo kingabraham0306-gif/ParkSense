3907a53013588e72a0b1e08b16ebf52da6fd5724
kingabraham0306-gif <kingabraham0306@gmail.com>
2026-04-01 19:02:17 +0300

    Add route to get all parking statuses

--- src/routes/ParkingRout.ts
+++ src/routes/ParkingRout.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { upsertParkingStatus, getParkingStatus } from "../controllers/ParkingController";
+
+import {
+  upsertParkingStatus,
+  getParkingStatus,
+  getAllParkingStatuses,
+} from "../controllers/ParkingController";
 
 const parkingRoutes = Router();
 
 parkingRoutes.post("/", upsertParkingStatus);
+
+// :white_check_mark: THIS IS THE FIX
+parkingRoutes.get("/", getAllParkingStatuses);
+
+// :exclamation: MUST BE BELOW
 parkingRoutes.get("/:name", getParkingStatus);
 
 export default parkingRoutes;
